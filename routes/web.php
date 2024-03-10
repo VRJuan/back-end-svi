328b052a59b1619990aaa8e3945902f9ee70c30a
Juan Vivas <jj.vivas>
2024-03-10 16:49:11 -0500

    ajuste de cors para comunicacion con el front-end

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return response('api v1')->header('Content-Type', 'text/plain');
 });
+Route::options('/public/sanctum/csrf-cookie', function () {
+    return response('', 200)
+        ->header('Access-Control-Allow-Origin', 'http://localhost:5173')
+        ->header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE')
+        ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+});
